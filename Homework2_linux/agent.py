152fe2b10eeec2a6cc70d25ae87bae639f381567
Bobyue <465741287@qq.com>
2019-09-27 13:46:26 +0800

    Add files via upload

---
+++ Homework2_linux/agent.py
@@ -0,0 +1,54 @@
+import random, re, datetime
+
+
+class Agent(object):
+    def __init__(self, game):
+        self.game = game
+
+    def getAction(self, state):
+        raise Exception("Not implemented yet")
+
+
+class RandomAgent(Agent):
+    def getAction(self, state):
+        legal_actions = self.game.actions(state)
+        self.action = random.choice(legal_actions)
+
+
+class SimpleGreedyAgent(Agent):
+    # a one-step-lookahead greedy agent that returns action with max vertical advance
+    def getAction(self, state):
+        legal_actions = self.game.actions(state)
+
+        self.action = random.choice(legal_actions)
+
+        player = self.game.player(state)
+        if player == 1:
+            max_vertical_advance_one_step = max([action[0][0] - action[1][0] for action in legal_actions])
+            max_actions = [action for action in legal_actions if
+                           action[0][0] - action[1][0] == max_vertical_advance_one_step]
+        else:
+            max_vertical_advance_one_step = max([action[1][0] - action[0][0] for action in legal_actions])
+            max_actions = [action for action in legal_actions if
+                           action[1][0] - action[0][0] == max_vertical_advance_one_step]
+        self.action = random.choice(max_actions)
+
+
+class TeamNameMinimaxAgent(Agent):
+    def getAction(self, state):
+        legal_actions = self.game.actions(state)
+        self.action = random.choice(legal_actions)
+
+        player = self.game.player(state)
+        ### START CODE HERE ###
+
+
+
+
+
+
+
+        ### END CODE HERE ###
+
+
+
